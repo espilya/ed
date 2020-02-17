3c0f061c81ac7fc1e6213d2066ca43f5507af631
espilya <espilya@gmail.com>
2020-02-17 15:32:37 +0100

    Add files via upload

---
+++ 4/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include "conjunto.h"
+
+using namespace std;
+
+int main(){
+   
+
+
+
+
+
+}
+
---
+++ 4/polinomio.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+#include <vector>
+
+using namespace std;
+
+typedef struct {
+	int coef;
+	int exp;
+
+}tMonomio;
+
+class polinomio {
+	
+	tMonomio t;
+
+	vector <tMonomio> v;
+
+public:
+
+	polinomio() {}	
+
+
+	void addMonimio(int c, int e) {
+		t.coef = c; t.exp = e;
+		v.push_back(t);
+	}	
+
+
+	int evaluar(int valor) {
+		for (int i = 0; i < v.size(); i++){
+
+		}
+	}
+
+};
