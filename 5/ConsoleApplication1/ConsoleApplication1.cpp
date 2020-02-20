28e46dc454ffd73ed0b186c40956e58100e84c0b
espilya <espilya@gmail.com>
2020-02-20 18:31:03 +0100

    /

---
+++ 5/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <limits.h>
+#include "complejos.h"
+using namespace std;
+
+
+
+//
+//3 2 6 1 2 0 0 0
+//2
+//-2 2
+
+
+int main() {
+	while (1) {
+		complejos<int, double> c(1, 2);
+
+
+
+}
+
---
+++ 5/ConsoleApplication1/complejos.h
@@ -0,0 +1,22 @@
+﻿
+#ifndef complejos_h
+#define complejos_h
+
+
+template <typename T1, typename T2>
+class complejos {
+	T1 _primero; 
+	T2 _segundo;
+
+
+public:
+	// Constructor
+
+	complejos(T1 a, T2 b) : _primero(a), _segundo(b) { }
+	// Observadoras
+	T1 primero() const { return _primero; }
+	T2 segundo() const { return _segundo; }
+};
+
+
+#endif // conjunto_h
